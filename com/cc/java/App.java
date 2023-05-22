6f398b59913cdf3999d41ff0880eb8cef8ace71f
Ade <adeyinkacomcave@gmail.com>
2023-05-22 11:34:41 +0200

    java.++

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -2,7 +2,13 @@ package com.cc.java;
 
 public class App {
     
+     public static void name(String[] args) {
+     
+        new Cat();
+        
+        System.out.println(new Cat());
     
+    } 
 
 }
 
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,11 @@
+package com.cc.java;
+
+public class Cat {
+    
+  public void tellYourAddress() {
+    System.out.println("My address: " + this);
+    
+  }
+
+
+}
